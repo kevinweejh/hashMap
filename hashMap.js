09abf391e4c63b8118e09a31766c904429bd7d7e
Wee Jian Hui Kevin <hello@codebykevin.dev>
2024-02-16 14:41:37 +0800

    fix: initiate independent arrays for each bucket

--- hashMap.js
+++ hashMap.js
@@ -2,13 +2,13 @@ const BUCKETS = 16;
 
 export class HashMap {
     constructor() {
-        this.buckets = new Array(BUCKETS).fill(null);
+        this.buckets = Array.from({ length: BUCKETS }, () => []);
     }
 
     hash = (key) => {
         let hashCode = 0;
-
         const primeNumber = 31;
+        
         for (let i = 0; i < key.length; i++) {
             hashCode = primeNumber * hashCode + key.charCodeAt(i);
         }
